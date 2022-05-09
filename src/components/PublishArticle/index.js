1fa9eaa80703baae6dde5d46e329cf105d6abeb0
Axel Ehrlich <53488067+axelehrlich59@users.noreply.github.com>
2022-05-09 16:44:14 +0200

    Add feature not available function

--- src/components/PublishArticle/index.js
+++ src/components/PublishArticle/index.js
@@ -5,6 +5,7 @@ import Button from "../Button/Index";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import backgroundImg from "../../assets/educational-bg.jpg"
+import { featureNotAvailableYet } from "../../utils/functions"
 
 const MainContainerPublicationArticle = styled.div`
   display: flex;
@@ -13,7 +14,6 @@ const MainContainerPublicationArticle = styled.div`
   height: 92%;
   background: url(${backgroundImg});
 `
-
 const PublicationArticleContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -26,7 +26,6 @@ const PublicationButtonContainer = styled.div`
   display: flex;
   align-items: center;
 `
-
 const HidePublicationContainer = styled.div`
   display: flex;
   justify-content: center;
@@ -64,6 +63,7 @@ const PublishArticle = () => {
               hideBorder={true}
               height={"40px"}
               width={"140px"}
+              onClick={featureNotAvailableYet}
             />
           </PublicationButtonContainer>
         </PublicationArticleContainer>
