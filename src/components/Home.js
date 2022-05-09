c7b44e45f2dfac2151a2af216bda0a0e1c3ea768
Axel Ehrlich <53488067+axelehrlich59@users.noreply.github.com>
2022-05-09 14:06:39 +0200

    Separate homepage in new folder

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,8 @@
 import React, {useState} from "react";
 import styled from 'styled-components'
-import PublishArticle from "./PublishArticle";
-import AvatarImg from "../assets/img_avatar3.png"
 import backgroundImg from "../assets/educational-bg.jpg"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faE, faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import AvatarImg from "../assets/img_avatar3.png"
+import ArticleDisplay from "./ArticleDisplay";
 
 
 const MainContainerArticlesPublished = styled.div`
@@ -18,106 +16,6 @@ const MainContainerArticlesPublished = styled.div`
   overflow: auto;
   background: url(${backgroundImg});
 `
-const ContainerArticle = styled.div`
-  display: flex;
-  height: 650px;
-  width: 1000px;
-  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
-  border-radius: 15px;
-  background-color: white;
-  @media screen and (max-width: 720px) {
-    width: 280px;
-    justify-content: center;
-  }
-  @media (min-width: 720px) and (max-width: 1024px) {
-    width: 500px;
-  }
-`
-const ContainerPictureArticle = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  height: 650px;
-  width: 200px;
-  border-top-left-radius: 15px;
-  border-bottom-left-radius: 15px;
-  @media screen and (max-width: 720px) {
-    width: 280px;
-  }
-  @media (min-width: 768px) and (max-width: 1024px) {
-    width: 90%;
-  }
-`
-const ContainerProfilElements = styled.div`
-  display: flex;
-  height: 200px;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-`
-const ContainerUserName = styled.div`
-  font-weight: bold;
-  font-size: 20px;
-  color: black;
-`
-const ContainerTextArticle = styled.div`
-  display: flex;
-  height: 630px;
-  width: 700px;
-  padding: 10px;
-  font-size: 18px;
-  font-weight: lighter;
-`
-const Img = styled.img`
-  width: 120px;
-  height: 120px;
-  border-radius: 50%;
-`
-const ContainerCrud = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  justify-content: space-between;
-  width: 80px;
-  height: 70px;
-  margin-top: 20px;
-`
-const ContainerIconTrash = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 20px;
-  padding: 5px;
-  box-shadow: rgba(0, 0, 0, 0.19) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-  border-radius: 5px;
-  :hover {
-    transform: translateY(-2px);
-    box-shadow: rgba(0, 0, 0, 0.12) 0px 10px 10px, rgba(0, 0, 0, 0.13) 0px 2px 2px;
-    transition: 0.6s;
-  }
-  cursor: pointer;
-`
-const ContainerIconEdit = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 20px;
-  padding: 5px;
-  box-shadow: rgba(0, 0, 0, 0.19) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-  border-radius: 5px;
-  :hover {
-    transform: translateY(-2px);
-    box-shadow: rgba(0, 0, 0, 0.12) 0px 10px 10px, rgba(0, 0, 0, 0.13) 0px 2px 2px;
-    transition: 0.6s;
-  }
-  cursor: pointer;
-`
-const IconTrash = styled(FontAwesomeIcon)`
-  color: #F16C63;
-`
-const IconEdit = styled(FontAwesomeIcon)`
-  color: #5c95ff;
-`
 
 
 
@@ -128,35 +26,14 @@ const Home = () => {
   return (
     <>
     <MainContainerArticlesPublished>
-      <ContainerArticle>
-        <ContainerPictureArticle>
-          <ContainerProfilElements>
-            <Img src={AvatarImg}/>
-            <ContainerUserName>Axel</ContainerUserName>
-          </ContainerProfilElements>
-        </ContainerPictureArticle>
-        <ContainerTextArticle>
-          {loremIpsum}
-        </ContainerTextArticle>
-        <ContainerCrud>
-          <ContainerIconTrash>
-            <IconTrash icon={faTrash} />
-          </ContainerIconTrash>
-          <ContainerIconEdit>
-            <IconEdit icon={faEdit} />
-          </ContainerIconEdit>
-        </ContainerCrud>
-      </ContainerArticle>
+      <ArticleDisplay 
+        text={loremIpsum}
+        username={"Axel"}
+        profilePicture={AvatarImg}
+      />
     </MainContainerArticlesPublished>
     <MainContainerArticlesPublished>
-      <ContainerArticle>
-      <ContainerPictureArticle>
-        <ContainerProfilElements>
-          <Img src={AvatarImg}/>
-          <ContainerUserName>Jean</ContainerUserName>
-        </ContainerProfilElements>
-      </ContainerPictureArticle>
-      </ContainerArticle>
+      <ArticleDisplay />
     </MainContainerArticlesPublished>
     </>
   )
